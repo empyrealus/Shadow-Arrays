181529ad33bb9ef1310f67ca7c9b735577ccfd85
Empyrealus <103290689+empyrealus@users.noreply.github.com>
2023-03-10 00:43:18 -0600

    Create shadow.js

---
+++ shadow.js
@@ -0,0 +1,21 @@
+
+function getSecondLargest(nums) {
+    let arr_start = 0, final_value, occurence = 0;
+    const init = () => {(!nums.sort()) ? null : (function () {
+          arr_start += 1;
+          function compare(a, b){return a - b;}
+          let sorted = nums.sort(compare); //encapsulated shadow
+          if(sorted.includes(sorted[arr_start])){
+            function occurenceAdd(value){occurence += 1;
+            if(sorted[arr_start-1] === sorted[arr_start]){;   
+               let index = sorted.indexOf(sorted[arr_start-1]);
+               sorted.splice(index, 1);occurence += 1;}
+            if(occurence >= 2){sorted.sort(compare);occurence = 0;}
+             return (occurence >= 2) ? null : value;}
+              sorted = sorted.filter(occurenceAdd);}
+           if(arr_start === nums.length){
+             final_value = sorted[sorted.length-2];}
+        })();}
+     let myNumber = nums.map(values => nums.every(init));
+    return final_value;
+}
